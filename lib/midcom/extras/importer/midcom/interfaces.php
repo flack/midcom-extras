d5489e5f3da13dfa759b2afcd15f6b02314f981d
flack <flack@contentcontrol-berlin.de>
2013-05-17 23:12:38 +0200

    Move importers to extras repo

---
+++ lib/midcom/extras/importer/midcom/interfaces.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default MidCOM interface class.
+ *
+ * @package midcom.extras.importers
+ */
+class midcom_extras_importers_interface extends midcom_baseclasses_components_interface
+{
+}
+?>
